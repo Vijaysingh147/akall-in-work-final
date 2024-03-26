4d5b59db2004cc52d003c236ed2f1b526d37b1db
Vijaysingh147 <161139887+Vijaysingh147@users.noreply.github.com>
2024-03-26 14:40:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = "6539249772:AAFUzL7ko9cnC4fyQ-2XyvEglXWPN0D0ix8"
+    BOT_TOKEN = "7050940877:AAGvrZp-m-gcE9CCRElKZItiXMi5aTtcTLU"
     API_ID = "23004101"
     API_HASH = "a2e157e87728053027cbb156e41a832a"
     AUTH_USERS = '5409975736'
